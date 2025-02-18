d04b977de26367a3333349fe6e35f9528b1777e5
DANN688 <1152899490@qq.com>
2025-02-18 13:31:58 +0800

    Create Settings.tsx

---
+++ frontend/src/pages/Settings.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Card } from "antd";
+
+const Settings: React.FC = () => {
+  return <Card title="系统设置"><p>这里是系统设置页面内容</p></Card>;
+};
+
+export default Settings;
