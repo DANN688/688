fb3de3168973ed3b75fcf3732102ea543a38f8b4
DANN688 <1152899490@qq.com>
2025-02-18 13:30:28 +0800

    Create Inventory.tsx

---
+++ frontend/src/pages/Inventory.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Card } from "antd";
+
+const Inventory: React.FC = () => {
+  return <Card title="库存管理"><p>这里是库存管理页面内容</p></Card>;
+};
+
+export default Inventory;
