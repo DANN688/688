e983ac50f56f8348729f1dd7deb962b93d337344
DANN688 <1152899490@qq.com>
2025-02-18 13:31:05 +0800

    Create Suppliers.tsx

---
+++ frontend/src/pages/Suppliers.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Card } from "antd";
+
+const Suppliers: React.FC = () => {
+  return <Card title="供应商管理"><p>这里是供应商管理页面内容</p></Card>;
+};
+
+export default Suppliers;
